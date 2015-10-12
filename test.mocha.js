43914c8d4e9c6991437332aa74a0a9cf036095ce
Nicolas Froidure <nicolas.froidure@insertafter.com>
2015-10-12 12:53:35 +0200

    Suppress eslint warnings

--- test.mocha.js
+++ test.mocha.js
@@ -6,48 +6,47 @@ var assert = require('assert');
 describe('objectid-stub', function() {
   var objectIdStub;
 
+  describe('without options', function() {
 
-    describe('without options', function() {
-
-      beforeEach(function() {
-        objectIdStub = initObjectIdGenerator();
-      });
-
-      it('should work as expected', function() {
-        assert.equal(objectIdStub(), 'abbacaca6a6affffffffffff');
-
-        assert.equal(objectIdStub.next(), objectIdStub());
-
-        assert.equal(objectIdStub(), 'abbacaca6a6afffffffffffd');
-
-        objectIdStub.reset();
-
-        assert.equal(objectIdStub(), 'abbacaca6a6affffffffffff');
-      });
-
+    beforeEach(function() {
+      objectIdStub = initObjectIdGenerator();
     });
 
-    describe('with prefix option', function() {
+    it('should work as expected', function() {
+      assert.equal(objectIdStub(), 'abbacaca6a6affffffffffff');
 
-      beforeEach(function() {
-        objectIdStub = initObjectIdGenerator({
-          prefix: '570b570b570b',
-        });
-      });
+      assert.equal(objectIdStub.next(), objectIdStub());
 
-      it('should work as expected', function() {
-        assert.equal(objectIdStub(), '570b570b570bffffffffffff');
+      assert.equal(objectIdStub(), 'abbacaca6a6afffffffffffd');
 
-        assert.equal(objectIdStub.next(), objectIdStub());
+      objectIdStub.reset();
 
-        assert.equal(objectIdStub(), '570b570b570bfffffffffffd');
-
-        objectIdStub.reset('570b570b570b');
-
-        assert.equal(objectIdStub(), '570b570b570bffffffffffff');
-      });
-
+      assert.equal(objectIdStub(), 'abbacaca6a6affffffffffff');
     });
+
+  });
+
+  describe('with prefix option', function() {
+
+    beforeEach(function() {
+      objectIdStub = initObjectIdGenerator({
+        prefix: '570b570b570b',
+      });
+    });
+
+    it('should work as expected', function() {
+      assert.equal(objectIdStub(), '570b570b570bffffffffffff');
+
+      assert.equal(objectIdStub.next(), objectIdStub());
+
+      assert.equal(objectIdStub(), '570b570b570bfffffffffffd');
+
+      objectIdStub.reset('570b570b570b');
+
+      assert.equal(objectIdStub(), '570b570b570bffffffffffff');
+    });
+
+  });
 
   describe('with ctor option', function() {
 
